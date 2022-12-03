056ede9b61501937196c65f0bcf242d59faf2b4c
Đào Tần <tocchien2003zx@gmail.com>
2022-12-04 04:46:38 +0700

    Validate form Trang Chủ, Login, BanHang_Demo,NhanVien

---
+++ 1.DAL/Migrations/20221203142909_3122022.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _1.DAL.Migrations
+{
+    public partial class _3122022 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HoaDon_NhanVien_IdNhanVien",
+                table: "HoaDon");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TrangThai",
+                table: "HoaDon",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "IdNhanVien",
+                table: "HoaDon",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HoaDon_NhanVien_IdNhanVien",
+                table: "HoaDon",
+                column: "IdNhanVien",
+                principalTable: "NhanVien",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HoaDon_NhanVien_IdNhanVien",
+                table: "HoaDon");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TrangThai",
+                table: "HoaDon",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "IdNhanVien",
+                table: "HoaDon",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HoaDon_NhanVien_IdNhanVien",
+                table: "HoaDon",
+                column: "IdNhanVien",
+                principalTable: "NhanVien",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- 3.PL/View/FrmLogin.cs
+++ 3.PL/View/FrmLogin.cs
@@ -5,46 +5,89 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _2.BUS.IServices;
+using _2.BUS.Services;
 
 namespace _3.PL
 {
     public partial class FrmLogin : Form
     {
+        private INhanVienService _nhanVienService;
         public FrmLogin()
         {
             InitializeComponent();
+            _nhanVienService = new NhanVienService();
         }
 
-        private string taiKhoan = "admin";
-        private string matKhau = "admin";
-        public void CheckDK()
+
+        public bool CheckDK()
         {
-            if (txt_TaiKhoan.Text == "" && txt_MatKhau.Text == "")
+            if (string.IsNullOrEmpty(txt_TaiKhoan.Text))
             {
-                MessageBox.Show("Tài khoản hoặc mật khẩu không được để trống","Thông báo",MessageBoxButtons.OKCancel,MessageBoxIcon.Question);
-                txt_TaiKhoan.Clear();
-                txt_MatKhau.Clear();
+                MessageBox.Show("Tài khoản không được để trống", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                return false;
             }
-            else if(txt_TaiKhoan.Text != this.taiKhoan && txt_MatKhau.Text != this.matKhau)
+            if (string.IsNullOrEmpty(txt_MatKhau.Text))
             {
-                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                MessageBox.Show("Mật khẩu không được để trống", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                return false;
+            }
+            if (txt_TaiKhoan.Text.Length <= 3)
+            {
+                MessageBox.Show("email bạn nhập không hợp lệ", "Thông báo");
+                return false;
+            }
+            if (Regex.IsMatch(txt_TaiKhoan.Text, @"(@)(.+)$") == false)
+            {
+
+                MessageBox.Show("Email không hợp lệ", "Thông báo");
+                return false;
+            }
+            if (txt_MatKhau.Text.Length <= 3)
+            {
+                MessageBox.Show("Mật khẩu bạn nhập không hợp lệ", "Thông báo");
+                return false;
+            }
+            if (Regex.IsMatch(txt_TaiKhoan.Text, @"(@)(.+)$") == false)
+            {
+
+                MessageBox.Show("Tài khoản phải nhập đúng định dang", "Thông báo");
+                return false;
             }
             else
             {
-                MessageBox.Show("Đăng nhập thành công","Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
-                FrmTrangChu tc = new FrmTrangChu();
-                tc.ShowDialog();
-                this.Hide();
-
+                return true;
             }
 
         }
 
         private void btn_DangNhap_Click(object sender, EventArgs e)
         {
-            CheckDK();
+            if (CheckDK() == false)
+            {
+                return;
+            }
+            else
+            {
+                foreach (var x in _nhanVienService.GetAll())
+                {
+                    if (x.Email == txt_TaiKhoan.Text && x.PassWord == txt_MatKhau.Text)
+                    {
+                        MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+                        FrmTrangChu tc = new FrmTrangChu(txt_TaiKhoan.Text);
+                        tc.ShowDialog();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tài khoản hoặc mật khẩu không đúng", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                    }
+                    return;
+                }
+            }
         }
     }
 }
