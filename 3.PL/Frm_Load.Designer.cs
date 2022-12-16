84b73c3d8caf1827aa56600c72e107f0edf9280a
nhokonhodao <tandtph20945@fpt.edu.vn>
2022-12-16 22:23:08 +0700

    Căn giữa các form Add nút thoát tắt chương trình form login

--- 3.PL/Frm_Load.Designer.cs
+++ 3.PL/Frm_Load.Designer.cs
@@ -39,18 +39,20 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(238, 64);
+            this.pictureBox1.Location = new System.Drawing.Point(272, 85);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(371, 268);
+            this.pictureBox1.Size = new System.Drawing.Size(424, 357);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(292, 350);
+            this.progressBar1.Location = new System.Drawing.Point(334, 467);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.progressBar1.MarqueeAnimationSpeed = 10;
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(278, 23);
+            this.progressBar1.Size = new System.Drawing.Size(318, 31);
             this.progressBar1.TabIndex = 1;
             // 
             // timer1
@@ -60,14 +62,16 @@
             // 
             // Frm_Load
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(822, 462);
+            this.ClientSize = new System.Drawing.Size(939, 616);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Frm_Load";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Frm_Load_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
